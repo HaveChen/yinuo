8074cf67d2f843cb4c5bc05dc6e1474d67ba1891
HaveChen <1065966570@qq.com>
2020-07-08 18:39:31 +0800

    更新到4.3.1

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysDept.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysDept.java
@@ -1,189 +1,185 @@
 package com.ruoyi.system.domain;
 
-import com.ruoyi.common.core.domain.BaseEntity;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 部门表 sys_dept
- *
+ * 
  * @author ruoyi
  */
-public class SysDept extends BaseEntity {
+public class SysDept extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
 
-  private static final long serialVersionUID = 1L;
+    /** 部门ID */
+    private Long deptId;
 
-  /** 部门ID */
-  private Long deptId;
+    /** 父部门ID */
+    private Long parentId;
 
-  /** 父部门ID */
-  private Long parentId;
+    /** 祖级列表 */
+    private String ancestors;
 
-  /** 祖级列表 */
-  private String ancestors;
+    /** 部门名称 */
+    private String deptName;
 
-  /** 部门名称 */
-  private String deptName;
+    /** 显示顺序 */
+    private String orderNum;
 
-  /** 部门代码 */
-  private String deptCode;
+    /** 负责人 */
+    private String leader;
 
-  /** 部门类型 */
-  private Integer deptType;
+    /** 联系电话 */
+    private String phone;
 
-  /** 显示顺序 */
-  private String orderNum;
+    /** 邮箱 */
+    private String email;
 
-  /** 负责人 */
-  private String leader;
+    /** 部门状态:0正常,1停用 */
+    private String status;
 
-  /** 联系电话 */
-  private String phone;
+    /** 删除标志（0代表存在 2代表删除） */
+    private String delFlag;
 
-  /** 邮箱 */
-  private String email;
+    /** 父部门名称 */
+    private String parentName;
 
-  /** 部门状态:0正常,1停用 */
-  private String status;
+    public Long getDeptId()
+    {
+        return deptId;
+    }
 
-  /** 删除标志（0代表存在 2代表删除） */
-  private String delFlag;
+    public void setDeptId(Long deptId)
+    {
+        this.deptId = deptId;
+    }
 
-  /** 父部门名称 */
-  private String parentName;
+    public Long getParentId()
+    {
+        return parentId;
+    }
 
-  public Long getDeptId() {
-    return deptId;
-  }
+    public void setParentId(Long parentId)
+    {
+        this.parentId = parentId;
+    }
 
-  public void setDeptId(Long deptId) {
-    this.deptId = deptId;
-  }
+    public String getAncestors()
+    {
+        return ancestors;
+    }
 
-  public Long getParentId() {
-    return parentId;
-  }
+    public void setAncestors(String ancestors)
+    {
+        this.ancestors = ancestors;
+    }
 
-  public void setParentId(Long parentId) {
-    this.parentId = parentId;
-  }
+    @NotBlank(message = "部门名称不能为空")
+    @Size(min = 0, max = 30, message = "部门名称长度不能超过30个字符")
+    public String getDeptName()
+    {
+        return deptName;
+    }
 
-  public String getAncestors() {
-    return ancestors;
-  }
+    public void setDeptName(String deptName)
+    {
+        this.deptName = deptName;
+    }
 
-  public void setAncestors(String ancestors) {
-    this.ancestors = ancestors;
-  }
+    @NotBlank(message = "显示顺序不能为空")
+    public String getOrderNum()
+    {
+        return orderNum;
+    }
 
-  public String getDeptCode() {
-    return deptCode;
-  }
+    public void setOrderNum(String orderNum)
+    {
+        this.orderNum = orderNum;
+    }
 
-  public void setDeptCode(String deptCode) {
-    this.deptCode = deptCode;
-  }
+    public String getLeader()
+    {
+        return leader;
+    }
 
-  public Integer getDeptType() {
-    return deptType;
-  }
+    public void setLeader(String leader)
+    {
+        this.leader = leader;
+    }
 
-  public void setDeptType(Integer deptType) {
-    this.deptType = deptType;
-  }
+    @Size(min = 0, max = 11, message = "联系电话长度不能超过11个字符")
+    public String getPhone()
+    {
+        return phone;
+    }
 
-  @NotBlank(message = "部门名称不能为空")
-  @Size(min = 0, max = 30, message = "部门名称长度不能超过30个字符")
-  public String getDeptName() {
-    return deptName;
-  }
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
 
-  public void setDeptName(String deptName) {
-    this.deptName = deptName;
-  }
+    @Email(message = "邮箱格式不正确")
+    @Size(min = 0, max = 50, message = "邮箱长度不能超过50个字符")
+    public String getEmail()
+    {
+        return email;
+    }
 
-  @NotBlank(message = "显示顺序不能为空")
-  public String getOrderNum() {
-    return orderNum;
-  }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 
-  public void setOrderNum(String orderNum) {
-    this.orderNum = orderNum;
-  }
+    public String getStatus()
+    {
+        return status;
+    }
 
-  public String getLeader() {
-    return leader;
-  }
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
 
-  public void setLeader(String leader) {
-    this.leader = leader;
-  }
+    public String getDelFlag()
+    {
+        return delFlag;
+    }
 
-  @Size(min = 0, max = 11, message = "联系电话长度不能超过11个字符")
-  public String getPhone() {
-    return phone;
-  }
+    public void setDelFlag(String delFlag)
+    {
+        this.delFlag = delFlag;
+    }
 
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
+    public String getParentName()
+    {
+        return parentName;
+    }
 
-  @Email(message = "邮箱格式不正确")
-  @Size(min = 0, max = 50, message = "邮箱长度不能超过50个字符")
-  public String getEmail() {
-    return email;
-  }
+    public void setParentName(String parentName)
+    {
+        this.parentName = parentName;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getStatus() {
-    return status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
-  }
-
-  public String getDelFlag() {
-    return delFlag;
-  }
-
-  public void setDelFlag(String delFlag) {
-    this.delFlag = delFlag;
-  }
-
-  public String getParentName() {
-    return parentName;
-  }
-
-  public void setParentName(String parentName) {
-    this.parentName = parentName;
-  }
-
-  @Override
-  public String toString() {
-    return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-        .append("deptId", getDeptId())
-        .append("parentId", getParentId())
-        .append("ancestors", getAncestors())
-        .append("deptCode", getDeptCode())
-        .append("deptType", getDeptType())
-        .append("deptName", getDeptName())
-        .append("orderNum", getOrderNum())
-        .append("leader", getLeader())
-        .append("phone", getPhone())
-        .append("email", getEmail())
-        .append("status", getStatus())
-        .append("delFlag", getDelFlag())
-        .append("createBy", getCreateBy())
-        .append("createTime", getCreateTime())
-        .append("updateBy", getUpdateBy())
-        .append("updateTime", getUpdateTime())
-        .toString();
-  }
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("deptId", getDeptId())
+            .append("parentId", getParentId())
+            .append("ancestors", getAncestors())
+            .append("deptName", getDeptName())
+            .append("orderNum", getOrderNum())
+            .append("leader", getLeader())
+            .append("phone", getPhone())
+            .append("email", getEmail())
+            .append("status", getStatus())
+            .append("delFlag", getDelFlag())
+            .append("createBy", getCreateBy())
+            .append("createTime", getCreateTime())
+            .append("updateBy", getUpdateBy())
+            .append("updateTime", getUpdateTime())
+            .toString();
+    }
 }
